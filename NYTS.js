ccd444af10ebbc6637c6d081148c2387cdefd65f
anvcodes <kpeace0720@gmail.com>
2019-10-07 11:46:09 -0400

    updates

--- NYTS.js
+++ NYTS.js
@@ -1,1 +1,23 @@
-javascript.js
+
+$(document).ready(function(){
+
+    var apiKey = "GtjLNczC810d4TJgtA4AwdApcoks5SHI"
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "&api-key=" + apiKey
+    var query = "SEARCH INPUT"
+    
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+    });
+
+})
+
+
+
+
+
+
+
+
